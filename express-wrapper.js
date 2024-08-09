9f517710401fbfef40e9cba986f9073aeeb8c48a
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-09 18:31:02 +0530

    Update express-wrapper.js

--- express-wrapper.js
+++ express-wrapper.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var bandwidthHeroProxy = require('./index.js').bandwidthHeroProxy;
+const express = require('express');
+const app = express();
+const bandwidthHeroProxy = require('./index.js').bandwidthHeroProxy;
 
 app.use('/bandwidth-hero', bandwidthHeroProxy);
 
